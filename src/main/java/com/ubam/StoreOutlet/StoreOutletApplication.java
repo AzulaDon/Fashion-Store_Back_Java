438f93fd2f89a137910eac33d8f63aa23cc216d0
Armando <armanditomart221205@gmail.com>
2026-03-18 16:45:28 -0600

    Nuevo proyecto

---
+++ src/main/java/com/ubam/StoreOutlet/StoreOutletApplication.java
@@ -0,0 +1,13 @@
+package com.ubam.StoreOutlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOutletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreOutletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubam/StoreOutlet/StoreOutletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubam.StoreOutlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOutletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
